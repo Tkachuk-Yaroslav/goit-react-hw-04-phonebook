932840103ef9e8f0cad4c44b887a351d72f913c9
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-09-19 16:34:53 +0300

    Beginning of transition to hooks (commented)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,33 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // import Modal from './Modal/Modal';
 // import Contact from './Contact/Contact';
 
+// import React from 'react';
+// import { useState } from 'react';
+
+// const App2 = () => {
+//   const [contacts, setContacts] = useState([
+//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//   ]);
+//   const [name, setName] = useState('');
+//   const [filter, setFilter] = useState('');
+
+//   const deleteContact = contactId => {
+//     setContacts(prev => {
+//       contacts: prev.contacts.filter(contact => contact.id !== contactId);
+//     });
+//     // this.setState(prevState => ({
+//     //   contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+//     // }));
+//   };
+
+//   return <div>App2</div>;
+// };
+
+// export default App2;
+
 class App extends Component {
   state = {
     contacts: [
